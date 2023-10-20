833050316cc9b63c8f8090bcceecaaa1f3a375fc
Maxim Kudryavtsev <maxim71ruswow@gmail.com>
2023-10-21 01:23:05 +0300

    feat: add testing requests

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import productionApi from '../api/production';
 
 export const Auth = () => {
+  const { data, isLoading } = useQuery('list', () => productionApi.getList({ skip: 0, take: 10 }));
+
   return <div>Hello AUTH</div>;
 };
