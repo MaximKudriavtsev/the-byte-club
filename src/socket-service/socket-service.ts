124322669d861f051aec39739c7266f5dabf6f94
Dmitry Morozov <mboddah@yandex.ru>
2023-10-22 12:17:07 +0300

    fix(client): fix server uri

--- src/socket-service/socket-service.ts
+++ src/socket-service/socket-service.ts
@@ -17,9 +17,9 @@ export function createSocketConnection() {
       broadcaster: 'pusher',
       key: 's2AG3U6DHq0Wtp4wq9Pw',
       cluster: 'mt1',
-      //wsHost: URL,
+      wsHost: URL,
       // wsHost: '80.78.207.182', // local
-      wsHost: '51.250.86.225', // prod
+      //wsHost: '51.250.86.225', // prod
       wsPort: 6001,
       disableStats: true,
       forceTLS: false,
