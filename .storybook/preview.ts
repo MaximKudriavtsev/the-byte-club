25168c54a8da332a7a52b005374e4111cdcd2ba0
Dmitry Morozov <mboddah@yandex.ru>
2023-11-18 19:05:22 +0300

    feature(client): add app backgrounds to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,28 @@
 import type { Preview } from '@storybook/react';
 import { decorators } from './../src/theme/theme';
+import { themeColors } from '../src/theme/config.theme';
+import { values } from 'lodash';
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+      default: 'app',
+      values: [
+        {
+          name: 'app',
+          value: themeColors.mainGradient,
+        },
+        {
+          name: 'green',
+          value: themeColors.green,
+        },
+        {
+          name: 'blue',
+          value: themeColors.blue,
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
